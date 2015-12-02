7f5118115e5e24c1e8038e4a6e920c09e2e79a20
alejandromartincruz <socorreitor@gmail.com>
2015-12-02 10:16:48 +0100

    add spiderfy to maps, fix high vew bug

--- app/assets/javascripts/geomaps.js
+++ app/assets/javascripts/geomaps.js
@@ -1,143 +1,214 @@
 'use strict'
-var map;
-var markers = [];
-var oms;
+  var map;
+  var oms;
 
-if ("geolocation" in navigator){
-  navigator.geolocation.getCurrentPosition(onLocation, onError);
-}
+  //Create info window (need only one)   
+  var infowindow = new google.maps.InfoWindow(); 
 
-function onLocation(position){
+  if ("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(onLocation, onError);
+  }
 
-  var myPosition = {
-    lat: position.coords.latitude,
-    lng: position.coords.longitude
-  };
+  function onLocation(position){
 
-  createMap(myPosition);
-  setupAutocomplete();
-}
+    var myPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
 
-function onError(err){
-  console.log("What browser are you using? IE 7??", err);
-}
-
-function createMap(position){
-  map = new google.maps.Map($('#map')[0], {
-    center: position,
-    zoom: 15,
-  });
-  yourPosition(position);
-
-  $( document ).ready(function() {
-    $.ajax({
-      url:"http://localhost:3000/events.json",
-      dataType: "json",
-      success: handleSucess,
-      error: handleError
-    });
-  });
-}
-
-function yourPosition(position){
-
-  var marker = new google.maps.Marker({
-    position: position,
-    animation: google.maps.Animation.DROP,
-    map: map,
-    icon: {
-    url: 'assets/your_pos.png',
-    scaledSize: new google.maps.Size(60, 60), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor
-    },          
-    title: "You are here",
-    color: "blue"
-  });
-};
-
-
-
-function createMarker(position, position_hash) {
-  //debugger;
-  console.log(position);
-  console.log(position_hash.title);
-  console.log(position_hash.description);
-
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">'+position_hash.title+'</h1>'+
-      '<div id="bodyContent">'+
-      '<p>'+position_hash.description+'</p>'+
-      '<p>'+ position_hash.date +'</p>'+
-      '<p>'+ position_hash.formatted_addres +'</p>'
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  if (moment(position_hash.date).isAfter(moment())) {
-
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      icon: "https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/48/Map-Marker-Marker-Inside-Azure.png",
-      animation: google.maps.Animation.DROP,
-      title: position_hash.title,
-    });
-
-    marker.addListener('click', function() {
+    createMap(myPosition);
+    oms = new OverlappingMarkerSpiderfier(map);
+    // listeners need to be registered only once
+    oms.addListener('click', function(marker, event) {
+      infowindow.setContent(marker.description);
       infowindow.open(map, marker);
     });
 
-    console.log("marker created");
-    markers.push(marker);
-    console.log(markers)
+    oms.addListener('spiderfy', function(markers) {
+      for(var i = 0; i < markers.length; i++) {
+        markers[i].setShadow(null);
+      } 
+      infowindow.close();
+    });
+
+    setupAutocomplete();
+  }
+
+  function createMap(position){
+    map = new google.maps.Map($('#map')[0], {
+      center: position,
+      zoom: 15,
+    });
+    yourPosition(position);
+
+    $( document ).ready(function() {
+      $.ajax({
+        url:"http://localhost:3000/events.json",
+        dataType: "json",
+        success: handleSucess,
+        error: handleError
+      });
+    });
+  }
+
+  //----------------------------------spyderfy-------------------------------->>
+
+    function handleSucess(data){
+      data.forEach(function(position_hash) {
+        if (moment(position_hash.date).isAfter(moment())) {
+          handleItem(position_hash);
+        };
+      });
+    };
+
+    function handleItem(position_hash){
+      //Info window content 
+      var contentString = '<div id="content">'+
+                          '<div id="siteNotice">'+
+                          '</div>'+
+                          '<h1 id="firstHeading" class="firstHeading">'+position_hash.title+'</h1>'+
+                          '<div id="bodyContent">'+
+                          '<p>'+position_hash.description+'</p>'+
+                          '<p>'+ position_hash.date +'</p>'+
+                          '<p>'+ position_hash.formatted_addres +'</p>'
+                          '</div>'+
+                          '</div>';
+
+      var img = 'http://www.google.com/mapfiles/marker.png';
+      var myLatLng = new google.maps.LatLng(position_hash.latitude, position_hash.longitude);
+      var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: "https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/48/Map-Marker-Marker-Inside-Azure.png"
+          });
+                    
+      // to be possible in "click" show specific content
+      marker.description = contentString;
+
+      oms.addMarker(marker);
+
+    };
+
+//---------------------------------------spyderfy----------------------------->>
+
+
+  function onError(err){
+    console.log("What browser are you using? IE 7??", err);
+  }
+
+  function createMap(position){
+    map = new google.maps.Map($('#map')[0], {
+      center: position,
+      zoom: 15,
+    });
+    yourPosition(position);
+
+    $( document ).ready(function() {
+      $.ajax({
+        url:"http://localhost:3000/events.json",
+        dataType: "json",
+        success: handleSucess,
+        error: handleError
+      });
+    });
+  }
+
+  function yourPosition(position){
+
+    var marker = new google.maps.Marker({
+      position: position,
+      animation: google.maps.Animation.DROP,
+      map: map,
+      icon: {
+      url: 'assets/your_pos.png',
+      scaledSize: new google.maps.Size(60, 60), // scaled size
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+      },          
+      title: "You are here",
+    });
   };
-};
 
+  /*
+  function createMarker(position, position_hash) {
+    //debugger;
+    console.log(position);
+    console.log(position_hash.title);
+    console.log(position_hash.description);
 
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">'+position_hash.title+'</h1>'+
+        '<div id="bodyContent">'+
+        '<p>'+position_hash.description+'</p>'+
+        '<p>'+ position_hash.date +'</p>'+
+        '<p>'+ position_hash.formatted_addres +'</p>'
+        '</div>'+
+        '</div>';
 
-  function handleSucess(data){
-    //displayCharacters(data);
-    data.forEach(function(position_hash) {
-
-      console.log(position_hash);
-      var eventPosition = {
-        lat: position_hash.latitude,
-        lng: position_hash.longitude
-      };
-      createMarker(eventPosition, position_hash);
-    });
-  }
-
-  function handleError(jqXHR, status, errorThrown){
-    alert("Something bad happened: "
-      + status + ', ' + errorThrown);
-  }
-
-
-function setupAutocomplete(){
-  var input = $('#location-placheholder')[0];
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.addListener('place_changed', function(){
-    var place = autocomplete.getPlace();
     var infowindow = new google.maps.InfoWindow({
+      content: contentString
     });
-    if (place.geometry.location) {
-      $("#location-longitude").val(place.geometry.location.lng().toFixed(7));
-      $("#location-longitude").trigger('input');
-      $("#location-latitude").val(place.geometry.location.lat().toFixed(7));
-      $("#location-latitude").trigger('input');
-      $(".filling").val(place.formatted_address);
-      $(".filling").trigger('input');
-      map.setCenter(place.geometry.location);
-      createMarker(place.geometry.location, place.formatted_address);
-    } else {
-      alert("The place has no location...?")
+
+    if (moment(position_hash.date).isAfter(moment())) {
+
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: "https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/48/Map-Marker-Marker-Inside-Azure.png",
+        animation: google.maps.Animation.DROP,
+        title: position_hash.title,
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+
+      console.log("marker created");
+      markers.push(marker);
+      console.log(markers)
+    };
+  };
+  
+
+
+    function handleSucess(data){
+      data.forEach(function(position_hash) {
+        console.log(position_hash);
+        var eventPosition = {
+          lat: position_hash.latitude,
+          lng: position_hash.longitude
+        };
+        createMarker(eventPosition, position_hash);
+      });
     }
-  });
-};
+  */
+
+    function handleError(jqXHR, status, errorThrown){
+      alert("Something bad happened: "
+        + status + ', ' + errorThrown);
+    }
+
+
+  function setupAutocomplete(){
+    var input = $('#location-placheholder')[0];
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function(){
+      var place = autocomplete.getPlace();
+      //var infowindow = new google.maps.InfoWindow({
+      //});
+      if (place.geometry.location) {
+        $("#location-longitude").val(place.geometry.location.lng().toFixed(7));
+        $("#location-longitude").trigger('input');
+        $("#location-latitude").val(place.geometry.location.lat().toFixed(7));
+        $("#location-latitude").trigger('input');
+        $(".filling").val(place.formatted_address);
+        $(".filling").trigger('input');
+        map.setCenter(place.geometry.location);
+        //createMarker(place.geometry.location, place.formatted_address);
+      } else {
+        alert("The place has no location...?")
+      }
+    });
+  };
